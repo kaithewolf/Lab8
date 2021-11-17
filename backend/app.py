1e553c40f9dfaa9de24b059aaa5f8a10d83dccdc
Adam Weingram <adamweingram@gmail.com>
2021-11-16 18:54:08 -0800

    Added skeleton code for the API

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,27 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
+# Application Routers
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home_handler():
+    return 'Please use the API at /api/v1/!'
+
+
+@app.route('/api/v1/', methods=['POST', 'GET'])
+def api_handler():
+    # Match request type
+    match request.method:
+        case 'GET':
+            print("GET")
+        case 'POST':
+            print("POST")
+        case _:
+            print("Unknown!")
 
 
 if __name__ == '__main__':
+
+    # Run the app
     app.run()
